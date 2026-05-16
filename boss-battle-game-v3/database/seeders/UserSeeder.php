b7eb8f8fa47cc29c51ac03ce8e25e3feb9d124ca
Aryo Wahyu Nugroho <aryowahyu24@gmail.com>
2026-05-16 16:52:27 +0700

    fix: new userseeder

--- boss-battle-game-v3/database/seeders/UserSeeder.php
+++ boss-battle-game-v3/database/seeders/UserSeeder.php
@@ -3,96 +3,65 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        // 5 Admin users
+        // 1 Admin
         User::create([
-            'nim' => '2241760001',
-            'nama' => 'Admin Utama',
-            'email' => 'admin@gmail.com',
-            'kelas' => null,
-            'role' => 'admin',
+            'nim'      => '2241760001',
+            'nama'     => 'Admin Utama',
+            'email'    => 'admin@gmail.com',
+            'kelas'    => null,
+            'role'     => 'admin',
             'total_xp' => 0,
-            'level' => 1,
-            'password' => bcrypt('password')
-        ]);
-        User::create([
-            'nim' => '2241760002',
-            'nama' => 'Admin Dosen',
-            'email' => 'dosen@gmail.com',
-            'kelas' => null,
-            'role' => 'admin',
-            'total_xp' => 0,
-            'level' => 1,
-            'password' => bcrypt('password')
+            'level'    => 1,
+            'password' => bcrypt('password'),
         ]);
 
-        // 3 Dosen users
+        // 2 Dosen
         User::create([
-            'nim' => 'NIDN001',
-            'nama' => 'Dr. Ahmad Fauzi',
-            'email' => 'ahmad.fauzi@dosen.ac.id',
-            'kelas' => null,
-            'role' => 'dosen',
+            'nim'      => 'NIDN001',
+            'nama'     => 'dosen1',
+            'email'    => 'dosen1@dosen.ac.id',
+            'kelas'    => null,
+            'role'     => 'dosen',
             'total_xp' => 0,
-            'level' => 1,
-            'password' => bcrypt('password')
+            'level'    => 1,
+            'password' => bcrypt('password'),
         ]);
 
         User::create([
-            'nim' => 'NIDN002',
-            'nama' => 'Siti Aminah, M.Kom',
-            'email' => 'siti.aminah@dosen.ac.id',
-            'kelas' => null,
-            'role' => 'dosen',
+            'nim'      => 'NIDN002',
+            'nama'     => 'dosen2',
+            'email'    => 'dosen2@dosen.ac.id',
+            'kelas'    => null,
+            'role'     => 'dosen',
             'total_xp' => 0,
-            'level' => 1,
-            'password' => bcrypt('password')
+            'level'    => 1,
+            'password' => bcrypt('password'),
         ]);
 
-        User::create([
-            'nim' => 'NIDN003',
-            'nama' => 'Budi Raharjo, S.Kom',
-            'email' => 'budi.raharjo@dosen.ac.id',
-            'kelas' => null,
-            'role' => 'dosen',
-            'total_xp' => 0,
-            'level' => 1,
-            'password' => bcrypt('password')
-        ]);
-
-        // 10 Student users
+        // 4 Students
         $students = [
-            ['nim' => '2241760101', 'nama' => 'aryod', 'kelas' => 'TI-3A'],
-            ['nim' => '2241760102', 'nama' => 'Siti Nurhaliza', 'kelas' => 'TI-3A'],
-            ['nim' => '2241760103', 'nama' => 'Budi Santoso', 'kelas' => 'TI-3B'],
-            ['nim' => '2241760104', 'nama' => 'Dewi Lestari', 'kelas' => 'TI-3B'],
-            ['nim' => '2241760105', 'nama' => 'Eko Prasetyo', 'kelas' => 'SIB-2A'],
-            ['nim' => '2241760106', 'nama' => 'Fitri Rahmawati', 'kelas' => 'SIB-2A'],
-            ['nim' => '2241760107', 'nama' => 'Gilang Ramadhan', 'kelas' => 'SIB-2B'],
-            ['nim' => '2241760108', 'nama' => 'Hana Pertiwi', 'kelas' => 'SIB-2B'],
-            ['nim' => '2241760109', 'nama' => 'Indra Wijaya', 'kelas' => 'TI-3A'],
-            ['nim' => '2241760110', 'nama' => 'Joko Susilo', 'kelas' => 'TI-3B'],
+            ['nim' => '2241760101', 'nama' => 'aryod1', 'kelas' => 'TI-3A'],
+            ['nim' => '2241760102', 'nama' => 'aryod2', 'kelas' => 'TI-3A'],
+            ['nim' => '2241760103', 'nama' => 'aryod3', 'kelas' => 'TI-3B'],
+            ['nim' => '2241760104', 'nama' => 'aryod4', 'kelas' => 'TI-3B'],
         ];
 
         foreach ($students as $student) {
             User::create([
-                'nim' => $student['nim'],
-                'nama' => $student['nama'],
-                'email' => strtolower(str_replace(' ', '.', $student['nama'])) . '@gmail.com',
-                'kelas' => $student['kelas'],
-                'role' => 'student',
+                'nim'      => $student['nim'],
+                'nama'     => $student['nama'],
+                'email'    => $student['nama'] . '@gmail.com',
+                'kelas'    => $student['kelas'],
+                'role'     => 'student',
                 'total_xp' => 0,
-                'level' => 1,
-                'password' => bcrypt('password')
+                'level'    => 1,
+                'password' => bcrypt('password'),
             ]);
         }
     }
